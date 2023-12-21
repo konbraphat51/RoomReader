71e28ab4b0ce49a3c3328c17289efb648597ec9c
konbraphat51 <konbraphat@gmail.com>
2023-12-21 09:02:12 +0900

    Add: easy setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="RoomReader",
+    version="0.0.1",
+    description="project_description",
+    url="https://github.com/author_name/project_urlname/",
+    long_description="a",
+    long_description_content_type="text/markdown",
+    author="author_name",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+#        "console_scripts": ["project_name = project_name.__main__:main"]
+    },
+)
