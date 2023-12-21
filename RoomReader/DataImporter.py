4d739e62fec114b2672eb1eb3eeb248d99214ad9
konbraphat51 <konbraphat@gmail.com>
2023-12-21 09:17:11 +0900

    debugging data

--- RoomReader/DataImporter.py
+++ RoomReader/DataImporter.py
@@ -7,8 +7,27 @@ from RoomReader.Config import Config
 from RoomReader.Vector import Vector
 from RoomReader.ImageData import ImageData
 
+#find maker note id
+for k, v in TAGS.items():
+    if v == "MakerNote":
+        MAKER_NOTE_ID = k
+        break
 
-
-
-def import_data(config: Config) -> list[ImageData]:
-    pass
+def import_picture(path: Path, config: Config) -> ImageData:
+    """Imports a picture from a given path and returns a ImageData object."""
+    image = Image.open(path.resolve())
+    
+    # Get the exif data
+    exif_data = image.getexif()
+    
+    for k, v in exif_data.items():
+        print(f"{TAGS.get(k)}: {v}")
+    
+    maker_note = exif_data[MAKER_NOTE_ID]
+    print(maker_note)
+    
+    image_data = ImageData(image)
+    
+    
+if __name__ == "__main__":
+    import_picture(Path("Data/image_2023-12-19T09-25-18.962Z.jpg"), Config())
