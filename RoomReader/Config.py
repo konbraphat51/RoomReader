df09d777100121761111172747bd6081218d68f0
konbraphat51 <konbraphat@gmail.com>
2023-12-21 11:17:56 +0900

    Fix: better file specification

--- RoomReader/Config.py
+++ RoomReader/Config.py
@@ -2,4 +2,5 @@ from pathlib import Path
 
 class Config:
     def __init__(self) -> None:
-        self.data_directory = Path("data")
+        self.library_directory = Path(__file__).parent
+        self.data_directory = self.library_directory / Path("Data")
